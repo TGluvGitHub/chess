2175a3b05b6392405d71e8fa190e6635e09ef750
TGluvGitHub <108703551+TGluvGitHub@users.noreply.github.com>
2022-07-05 12:37:01 -0700

    Delete window_code.py

--- window_code.py
+++
@@ -1,4 +0,0 @@
-import tkinter
-win=tkinter.Tk() #creating the main window and storing the window object in 'win'
-#We create the widgets here
-win.mainloop() #running the loop that works as a trigger
